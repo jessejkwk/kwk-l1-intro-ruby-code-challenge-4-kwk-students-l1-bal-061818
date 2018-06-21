901b3c6c7cf55b11b57c8fb22ea37bc2da817c1a
jessejkwk <janderson@nyatoday.com>
2018-06-21 17:52:45 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -6,18 +6,26 @@ $least_coins_one = {
 }
 
 def least_coins(cents)
-  q = cents / 25
-  q_r = (cents - 25*q)
-  d = (q_r)/10
-  d_r = (q_r - d*10)
-  n = (d_r)/5
-  n_r = (n)
-  if n_r == 1
-    pen = (n/1) - 1
+  if cents % 10 == 0
+    q = cents / 25
+    q_r = (cents - 25*q)
+    d = (q_r)/10
+    d_r = (q_r - d*10) 
+    n = (d_r)/5
+    n_r = (q_r - n*5)
+    if n_r == 0
+      pen = (n/1) - 1
+    else 
+      pen = (n/1)
+    end
   else
-    pen = (n/1)
+    q = cents / 25
+    q_r = (cents - 25*q)
+    d = (q_r)/10
+    d_r = (q_r - d*10) 
+    n = (d_r)/5
+    pen = (d_r)/1
   end
-  
   puts "The amount of quarters is #{q}"
   puts "The amount of dimes is #{d}"
   puts "The amount of nickels is #{n}"
